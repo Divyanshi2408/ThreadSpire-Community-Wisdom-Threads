39ba1715fb3775c82a2818ec6ee21c6a0c04a433
Divyanshi <85859444+Divyanshi2408@users.noreply.github.com>
2025-05-11 21:31:24 +0530

    change deploy url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "https://thread-spire-community-wisdom-threads.vercel.app/api";
+const API_BASE = "https://threadspire-community-wisdom-threads.onrender.com/api";
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem("token");
